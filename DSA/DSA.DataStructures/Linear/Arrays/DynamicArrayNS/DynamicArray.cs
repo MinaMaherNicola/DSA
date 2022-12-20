7cf6a5c48284a13c982579648afa65976c2e37bf
Mina Nicola <mina.nicola@evapharma.com>
2022-12-20 13:10:19 +0200

    added basics of C# project, removed Java project, and added basics of dynamic array

---
+++ DSA/DSA.DataStructures/Linear/Arrays/DynamicArrayNS/DynamicArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.DataStructures.Linear.Arrays.DynamicArrayNS
+{
+    public class DynamicArray<T>
+    {
+        private T[] array;
+
+        public DynamicArray()
+        {
+            array = Array.Empty<T>();
+        }
+
+        // O(1)
+        public int GetLength()
+        {
+            return array.Length;
+        }
+
+        // O(n)
+        public void Add(T item)
+        {
+            T[] newArray = new T[array.Length + 1];
+            array.CopyTo(newArray, 0);
+            newArray[^1] = item;
+            array = newArray;
+        }
+
+        // O(n)
+        public void Shift(T item)
+        {
+            T[] newArray = new T[array.Length + 1];
+            array.CopyTo(newArray, 1);
+            newArray[0] = item;
+            array = newArray;
+        }
+
+        // O(1)
+        public T FirstOrDefault()
+        {
+            return array[0];
+        }
+
+        // O(1)
+        public T LastOrDefault()
+        {
+            return array[^1];
+        }
+    }
+}
