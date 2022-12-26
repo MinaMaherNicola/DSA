aa0584fe23b2b6080cbf643477f99de14a975e18
MinaMaherNicola <minamahernicola@outlook.com>
2022-12-26 23:13:17 +0200

    finished single linked list implementation

--- DSA/DSA.DataStructures/Linear/LinkedLists/SingleLinkedListNS/ISingleLinkedList.cs
+++ DSA/DSA.DataStructures/Linear/LinkedLists/SingleLinkedListNS/ISingleLinkedList.cs
@@ -15,15 +15,33 @@ namespace DSA.DataStructures.Linear.LinkedLists.SingleLinkedListNS
         void Add(int item);
 
         // O(n)
+        void AddAfter(int target, int item);
+
+        // O(n)
+        void ShiftBefore(int target, int item);
+
+        // O(n)
         bool DoesExist(int item);
 
         // O(1)
         int GetLength();
 
         // O(n)
-        void DeleteItem(int item);
+        bool DeleteItem(int item);
 
         // O(1)
         void Clear();
+
+        // O(n)
+        int[] ToArray();
+
+        // O(n)
+        bool CompareToArray(int[] comparer);
+
+        // O(1)
+        int? FirstOrNull();
+
+        // O(1)
+        int? LastOrNull();
     }
 }
--- DSA/DSA.DataStructures/Linear/LinkedLists/SingleLinkedListNS/SingleLinkedList.cs
+++ DSA/DSA.DataStructures/Linear/LinkedLists/SingleLinkedListNS/SingleLinkedList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSA.DataStructures.Nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,34 +9,236 @@ namespace DSA.DataStructures.Linear.LinkedLists.SingleLinkedListNS
 {
     public class SingleLinkedList : ISingleLinkedList
     {
+        private SingleNode? head;
+        private SingleNode? tail;
+        private int length;
+
+        public SingleLinkedList()
+        {
+            head = default;
+            tail = default;
+            length = 0;
+        }
+
         public void Add(int item)
         {
-            throw new NotImplementedException();
+            if (IsListEmpty())
+            {
+                InitializeList(item);
+                return;
+            }
+            tail.Next = new SingleNode(item);
+            tail = tail.Next;
+            length++;
+        }
+
+        public void AddAfter(int target, int item)
+        {
+            if (IsListEmpty() || !DoesExist(target))
+            {
+                throw new Exception("Target is not found or LinkedList is empty!");
+            }
+            SingleNode cursor = head;
+
+            while (cursor.Data != target)
+            {
+                cursor = cursor.Next;
+            }
+            SingleNode newNode = new(item);
+
+            if (cursor.Next == null)
+            {
+                cursor.Next = newNode;
+                tail = newNode;
+                length++;
+                return;
+            }
+
+            newNode.Next = cursor.Next;
+            cursor.Next = newNode;
+            length++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            head = null;
+            tail = null;
+            length = 0;
         }
 
-        public void DeleteItem(int item)
+        public bool CompareToArray(int[] comparer)
         {
-            throw new NotImplementedException();
+            if (IsListEmpty() || comparer.Length != length)
+            {
+                return false;
+            }
+            SingleNode cursor = head;
+
+            for (int i = 0; i < comparer.Length; i++)
+            {
+                if (comparer[i] != cursor.Data)
+                {
+                    return false;
+                }
+                cursor = cursor.Next;
+            }
+            return true;
+        }
+
+        public bool DeleteItem(int item)
+        {
+            if (IsListEmpty() || !DoesExist(item))
+            {
+                return false;
+            }
+            if (length == 1)
+            {
+                Clear();
+                return true;
+            }
+            if (head.Data == item)
+            {
+                head = head.Next;
+                length--;
+                return true;
+            }
+
+            SingleNode cursor = head;
+
+            while (cursor.Next != tail && cursor.Next.Data != item)
+            {
+                cursor = cursor.Next;
+            }
+
+            if (cursor.Next == tail)
+            {
+                tail = cursor;
+                tail.Next = null;
+                length--;
+                return true;
+            }
+            cursor.Next = cursor.Next.Next;
+            length--;
+            return true;
         }
 
         public bool DoesExist(int item)
         {
-            throw new NotImplementedException();
+            if (IsListEmpty())
+            {
+                return false;
+            }
+            SingleNode cursor = head;
+
+            while (cursor != null)
+            {
+                if (cursor.Data == item)
+                {
+                    return true;
+                }
+                cursor = cursor.Next;
+            }
+            return false;
+        }
+
+        public int? FirstOrNull()
+        {
+            if (IsListEmpty())
+            {
+                return null;
+            }
+            return head?.Data;
         }
 
         public int GetLength()
         {
-            throw new NotImplementedException();
+            return length;
+        }
+
+        public int? LastOrNull()
+        {
+            if (IsListEmpty())
+            {
+                return null;
+            }
+            return tail?.Data;
         }
 
         public void Shift(int item)
         {
-            throw new NotImplementedException();
+            if (IsListEmpty())
+            {
+                InitializeList(item);
+                return;
+            }
+            head = new SingleNode(item)
+            {
+                Next = head
+            };
+            length++;
+        }
+
+        public void ShiftBefore(int target, int item)
+        {
+            if (IsListEmpty() || !DoesExist(target))
+            {
+                throw new Exception("\"Target is not found or LinkedList is empty!");
+            }
+
+            SingleNode newNode = new(item);
+
+            SingleNode cursor = head;
+
+            if (cursor.Data == target)
+            {
+                newNode.Next = head;
+                head = newNode;
+                length++;
+                return;
+            }
+
+            while (cursor.Next != null && cursor.Next.Data != target)
+            {
+                cursor = cursor.Next;
+            }
+
+            newNode.Next = cursor.Next;
+            cursor.Next = newNode;
+            length++;
+        }
+
+        public int[] ToArray()
+        {
+            if (IsListEmpty())
+            {
+                return Array.Empty<int>();
+            }
+            SingleNode cursor = head;
+            int[] output = new int[length];
+            
+            for (int i = 0; i < length; i++)
+            {
+                output[i] = cursor.Data;
+                cursor = cursor.Next;
+            }
+
+            return output;
+        }
+
+        private void InitializeList(int item)
+        {
+            head = new(item);
+            tail = head;
+            length++;
+        }
+
+        private bool IsListEmpty()
+        {
+            if (head == null || tail == null)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
