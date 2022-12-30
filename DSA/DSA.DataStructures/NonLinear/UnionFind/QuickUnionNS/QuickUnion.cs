bc25e0855fdf949d40ce1fc6357a79f54aecd74a
MinaMaherNicola <minamahernicola@outlook.com>
2022-12-30 21:46:06 +0200

    finished quickUnion algorithm

---
+++ DSA/DSA.DataStructures/NonLinear/UnionFind/QuickUnionNS/QuickUnion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.DataStructures.NonLinear.UnionFind.QuickUnionNS
+{
+    public class QuickUnion : IQuickUnion
+    {
+        private readonly int[] array;
+        public QuickUnion(int l)
+        {
+            array = new int[l];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+        }
+        public void Connect(int a, int b)
+        {
+            // 5aly al root bta3 a yb2a child ll root bta3 b
+            int aRoot = GetRoot(a);
+            int bRoot = GetRoot(b);
+
+            array[aRoot] = bRoot;
+        }
+
+        public bool Find(int a, int b)
+        {
+            int aRoot = GetRoot(a);
+            int bRoot = GetRoot(b);
+
+            if (aRoot == bRoot) return true;
+            return false;
+        }
+
+        public int Length()
+        {
+            return array.Length;
+        }
+
+        private int GetRoot(int i)
+        {
+            while (i != array[i])
+            {
+                i = array[i];
+            }
+            return i;
+        }
+    }
+}
---
+++ DSA/DSA.Tests/DataStructures/NonLinear/UnionFind/QuickUnionTestsNS/QuickUnionTests.cs
@@ -0,0 +1,65 @@
+﻿using DSA.DataStructures.NonLinear.UnionFind.QuickUnionNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DSA.Tests.DataStructures.NonLinear.UnionFind.QuickUnionTestsNS
+{
+    public class QuickUnionTests
+    {
+        [Fact]
+        public void Passing_Length_L_To_Quick_Union_Constructor_Should_Create_N_Nodes()
+        {
+            int length = 10;
+
+            QuickUnion quickUnion = new(length);
+
+            Assert.Equal(length, quickUnion.Length());
+        }
+
+        [Fact]
+        public void Using_Find_On_Connected_Elements_Should_Return_True_0()
+        {
+            QuickUnion quickUnion = new(10);
+
+            quickUnion.Connect(1, 2);
+            quickUnion.Connect(2, 3);
+            quickUnion.Connect(3, 4);
+            quickUnion.Connect(5, 8);
+            quickUnion.Connect(1, 8);
+
+            Assert.True(quickUnion.Find(2, 8));
+        }
+
+        [Fact]
+        public void Using_Find_On_Connected_Elements_Should_Return_True_1()
+        {
+            QuickUnion quickUnion = new(10);
+
+            quickUnion.Connect(1, 2);
+            quickUnion.Connect(2, 3);
+            quickUnion.Connect(3, 4);
+            quickUnion.Connect(5, 8);
+            quickUnion.Connect(1, 8);
+
+            Assert.True(quickUnion.Find(3, 5));
+        }
+
+        [Fact]
+        public void Using_Find_On_Unconnected_Elements_Should_Return_False()
+        {
+            QuickUnion quickUnion = new(10);
+
+            quickUnion.Connect(1, 2);
+            quickUnion.Connect(2, 3);
+            quickUnion.Connect(3, 4);
+            quickUnion.Connect(5, 8);
+            quickUnion.Connect(1, 8);
+
+            Assert.False(quickUnion.Find(1, 9));
+        }
+    }
+}
