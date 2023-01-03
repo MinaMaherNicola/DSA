ff83c8a59f3d0581e81d19a90ad701eeee09fbae
MinaMaherNicola <minamahernicola@outlook.com>
2023-01-03 16:36:43 +0200

    Solved 2.AddTwoNumber leetcode problem (need optimization)

---
+++ DSA/DSA.ProblemSolving/LeetCode/2.AddTwoNumbers/AddTwoNumbersSolution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.ProblemSolving.LeetCode._2.AddTwoNumbers
+{
+    public class AddTwoNumbersSolution
+    {
+        // O(n)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            BigInteger l1Value = GetListValuesReversed(l1);
+            BigInteger l2Value = GetListValuesReversed(l2);
+
+            BigInteger outputReversed = l1Value + l2Value;
+            ListNode head = new((int)(outputReversed % 10));
+            outputReversed /= 10;
+            ListNode cursor = head;
+
+            while (outputReversed > 0)
+            {
+                cursor.next = new((int)(outputReversed % 10));
+                cursor = cursor.next;
+                outputReversed /= 10;
+            }
+            return head;
+
+        }
+
+        // O(n)
+        public BigInteger GetListValuesReversed(ListNode l)
+        {
+            ListNode cursor = l;
+            string listAsString = "";
+            while (cursor != null)
+            {
+                listAsString += cursor.val;
+                cursor = cursor.next;
+            }
+            string listAsReversedString = "";
+            for (int i = listAsString.Length - 1; i >= 0; i--)
+            {
+                listAsReversedString += listAsString[i];
+            }
+            return BigInteger.Parse(listAsReversedString);
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ DSA/DSA.ProblemSolving/Program.cs
@@ -0,0 +1,16 @@
+﻿using DSA.ProblemSolving.LeetCode._2.AddTwoNumbers;
+
+namespace DSA.ProblemSolving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l1 = new(9);
+            ListNode l2 = new(1, new(9, new(9, new(9, new(9, new(9, new(9, new(9, new(9, new(9))))))))));
+
+            AddTwoNumbersSolution h5a = new();
+            h5a.AddTwoNumbers(l1, l2);
+        }
+    }
+}
