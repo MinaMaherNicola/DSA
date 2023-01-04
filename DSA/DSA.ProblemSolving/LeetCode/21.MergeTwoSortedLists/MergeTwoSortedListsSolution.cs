25513757958eb3349d416f0b2f8f9091f04285bf
MinaMaherNicola <minamahernicola@outlook.com>
2023-01-05 00:57:07 +0200

    solved leetcode 21.MergeTwoSortedLists

---
+++ DSA/DSA.ProblemSolving/LeetCode/21.MergeTwoSortedLists/MergeTwoSortedListsSolution.cs
@@ -0,0 +1,48 @@
+﻿using DSA.ProblemSolving.LeetCode._2.AddTwoNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.ProblemSolving.LeetCode._21.MergeTwoSortedLists
+{
+    public class MergeTwoSortedListsSolution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummyHead = new();
+            ListNode cursor = dummyHead;
+
+            while (list1 != null || list2 != null)
+            {
+                if (list1 == null)
+                {
+                    cursor.next = new(list2.val);
+                    cursor = cursor.next;
+                    list2 = list2.next;
+                    continue;
+                }
+                if (list2 == null)
+                {
+                    cursor.next = new(list1.val);
+                    cursor = cursor.next;
+                    list1 = list1.next;
+                    continue;
+                }
+                if (list1.val > list2.val)
+                {
+                    cursor.next = new(list2.val);
+                    cursor = cursor.next;
+                    list2 = list2.next;
+                    continue;
+                }
+                cursor.next = new(list1.val);
+                cursor = cursor.next;
+                list1 = list1.next;
+                continue;
+            }
+            return dummyHead.next;
+        }
+    }
+}
