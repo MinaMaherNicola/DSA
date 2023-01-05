9315e94354a920e6c7b5d32b30df39f4968e694b
MinaMaherNicola <minamahernicola@outlook.com>
2023-01-05 02:23:58 +0200

    solved leetcode 206.ReverseLinkedList

---
+++ DSA/DSA.ProblemSolving/LeetCode/206.ReverseLinkedList/ReverseLinkedListSolution.cs
@@ -0,0 +1,32 @@
+﻿using DSA.ProblemSolving.LeetCode._2.AddTwoNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.ProblemSolving.LeetCode._206.ReverseLinkedList
+{
+    public class ReverseLinkedListSolution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode left = null;
+            ListNode right = head;
+
+            while (right != null)
+            {
+                ListNode holder = right.next;
+                right.next = left;
+                left = right;
+                right = holder;
+            }
+            head = left;
+            return head;
+        }
+    }
+}
