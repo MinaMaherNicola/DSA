c2261570c15c7f1a4073b518f3480eddc0f9f01a
MinaMaherNicola <minamahernicola@outlook.com>
2023-01-10 08:00:38 +0200

    implemented separateChainingHashTable

---
+++ DSA/DSA.DataStructures/Linear/HashTables/SeparateChainingHashTableNS/SeparateChainingHashTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.DataStructures.Linear.HashTables.SeparateChainingHashTableNS
+{
+    public class SeparateChainingHashTable
+    {
+        private readonly LinkedList<string>[] hashtable;
+
+        public SeparateChainingHashTable()
+        {
+            hashtable = new LinkedList<string>[10];
+        }
+
+        public SeparateChainingHashTable(int l)
+        {
+            hashtable = new LinkedList<string>[l];
+        }
+
+        public void Add(string item)
+        {
+            int index = Hash(item);
+
+            if (hashtable[index] == null) hashtable[index] = new LinkedList<string>();
+            hashtable[index].AddLast(item);
+        }
+
+        public bool DoesExist(string item)
+        {
+            int index = Hash(item);
+            return hashtable[index] != null && hashtable[index].Contains(item);
+        }
+
+        private int Hash(string item)
+        {
+            int totalAsciiValue = 0;
+            foreach (char c in item)
+            {
+                totalAsciiValue += c;
+            }
+            return totalAsciiValue % hashtable.Length;
+        }
+    }
+}
---
+++ DSA/DSA.Tests/DataStructures/Linear/HashTables/SeparateChainingHashTableTestsNs/SeparateChainingHashTableTests.cs
@@ -0,0 +1,47 @@
+﻿using DSA.DataStructures.Linear.HashTables.SeparateChainingHashTableNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DSA.Tests.DataStructures.Linear.HashTables.SeparateChainingHashTableTestsNs
+{
+    public class SeparateChainingHashTableTests
+    {
+        private readonly SeparateChainingHashTable hashTable = new();
+
+        [Fact]
+        public void Using_Add_Should_Hash_The_Value_And_Add_It_To_Its_Key()
+        {
+            hashTable.Add("hello");
+
+            Assert.True(hashTable.DoesExist("hello"));
+        }
+
+        [Fact]
+        public void Using_DoesExist_With_Item_That_Doesnt_Exist_Should_Return_False()
+        {
+            Assert.False(hashTable.DoesExist("doe"));
+        }
+
+        [Fact]
+        public void Using_Add_That_Causes_Collision_Should_Chain_Values_Into_Same_List_0()
+        {
+            hashTable.Add("a");
+            hashTable.Add("@!");
+
+            Assert.True(hashTable.DoesExist("a"));
+        }
+
+        [Fact]
+        public void Using_Add_That_Causes_Collision_Should_Chain_Values_Into_Same_List_1()
+        {
+            hashTable.Add("a");
+            hashTable.Add("@!");
+
+            Assert.True(hashTable.DoesExist("@!"));
+        }
+    }
+}
