fb196298c478c744f2699b8e3b8f09ba60018e6a
MinaMaherNicola <minamahernicola@outlook.com>
2023-05-06 21:17:54 +0300

    implemented bubble sort

--- DSA/Algorithms/SortingAlgorithmsNS/SortingAlgorithms.cs
+++ DSA/Algorithms/SortingAlgorithmsNS/SortingAlgorithms.cs
@@ -15,5 +15,20 @@ namespace DSA.Algorithms.SortingAlgorithmsNS
       }
       return arr;
     }
+
+    public static int[] BubbleSort(int[] arr)
+    {
+      for (int i = 0; i < arr.Length; i++)
+      {
+        for (int j = 0; j < arr.Length - 1; j++)
+        {
+          if (arr[j] > arr[j + 1])
+          {
+            (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+          }
+        }
+      }
+      return arr;
+    }
   }
 }
